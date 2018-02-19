afb4709054e54a84ff828491d6c634819f2900ac
vijayC <vijaychendrayan@gmail.com>
2018-02-19 13:17:25 +0530

    Opera

--- cerner/automation/OperaWebDriver.java
+++ cerner/automation/OperaWebDriver.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.opera.OperaDriver;
 /**
  * Created by VC024129 on 5/6/2017.
  */
+ //Opera
 public class OperaWebDriver implements GenericWebDriver{
     public WebDriver setWebDriver(String prop, String path){
         System.setProperty(prop,path);
