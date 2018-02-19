cf5be3fc3522b6e127756b4553301b7aa93d3a18
vijayC <vijaychendrayan@gmail.com>
2018-02-19 20:34:13 +0530

    Test Push CLI

--- cerner/automation/OperaWebDriver.java
+++ cerner/automation/OperaWebDriver.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.opera.OperaDriver;
 /**
  * Created by VC024129 on 5/6/2017.
  */
- //Opera
+ //Opera12
 public class OperaWebDriver implements GenericWebDriver{
     public WebDriver setWebDriver(String prop, String path){
         System.setProperty(prop,path);
